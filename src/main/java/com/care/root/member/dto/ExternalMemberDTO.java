998691fc52567986702a38442cbc0363d7586476
geonmo.kim <kpunh1542@gmail.com>
2020-12-30 16:25:08 +0900

    카카오, 네이버 로그인 디비저장

---
+++ src/main/java/com/care/root/member/dto/ExternalMemberDTO.java
@@ -0,0 +1,20 @@
+package com.care.root.member.dto;
+
+public class ExternalMemberDTO {
+	private int e_id;
+	private String e_name;
+	public int getE_id() {
+		return e_id;
+	}
+	public void setE_id(int e_id) {
+		this.e_id = e_id;
+	}
+	public String getE_name() {
+		return e_name;
+	}
+	public void setE_name(String e_name) {
+		this.e_name = e_name;
+	}
+	
+	
+}
